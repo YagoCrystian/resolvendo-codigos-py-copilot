ee28fa52e9fa492a80921bb6fdccc09a2095817f
Yago Crystian <103502519+YagoCrystian@users.noreply.github.com>
2024-07-04 23:37:42 -0300

    Update ope_mat.py
    Concluido o desafio2

--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,17 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+numero1 = int(input("Digite o primeiro valor: "))
+numero2 = int(input("Digite o segundo valor: "))
+
+adicao = numero1 + numero2
+subtracao = numero1 - numero2
+divisao = numero1 / numero2
+multiplicao = numero1 * numero2
+
+print(f"Os valores digitados foram: {numero1} e {numero2}")
+
+print(f"Somando os dos dois valores obtemos: {adicao}")
+print(f"Subtraindo o primeiro valor do segundo obtemos: {subtracao}")
+print(f"Dividindo o primeiro valor do segundo obtemos: {divisao}")
+print(f"Multiplicando os dois valores obtemos: {multiplicacao}")
+
+print("Volte sempre!")
