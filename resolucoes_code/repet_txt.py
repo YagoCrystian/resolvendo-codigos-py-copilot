9e5ac43275dfe65520a74ef591a99b00f1349769
Yago Crystian <103502519+YagoCrystian@users.noreply.github.com>
2024-07-04 23:42:56 -0300

    Update repet_txt.py

--- resolucoes_code/repet_txt.py
+++ resolucoes_code/repet_txt.py
@@ -1,1 +1,6 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+palavra = input("Digite uma palavra: ")
+repeticoes = int(input("Digite um numero entre 1 e 100: "))
+
+print(palavra * repeticoes)
+print("Gostou da magica? tente novamente com palavras diferentes e numeros diferentes!")
