b4e0caced0c8c17b565b5cc8b62b0d309ada0855
Yago Crystian <103502519+YagoCrystian@users.noreply.github.com>
2024-07-04 23:30:38 -0300

    Update concat_dados.py
    Concluido o Desafio1

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,9 @@
-# Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+# Recebe dois dados diferentes do usuário
+dado_usuario1 = input("Digite o seu primeiro nome: ")
+dado_usuario2 = input("Digite o seu sobrenome: ")
+
+# Concatena os dados em uma única string
+nome_completo = f"{dado_usuario1} {dado_usuario2}"
+
+# Imprime a string concatenada
+print(f"O seu nome completo é {nome_completo}?")
